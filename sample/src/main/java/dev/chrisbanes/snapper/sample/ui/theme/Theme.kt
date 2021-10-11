5f2c7defb5ae94d40a0832e69fcb99d30939633a
Chris Banes <cbanes@twitter.com>
2021-10-11 14:51:23 +0100

    Add stub sample app

---
+++ sample/src/main/java/dev/chrisbanes/snapper/sample/ui/theme/Theme.kt
@@ -0,0 +1,36 @@
+package dev.chrisbanes.snapper.sample.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+@Composable
+fun SnapperTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        content = content,
+    )
+}
